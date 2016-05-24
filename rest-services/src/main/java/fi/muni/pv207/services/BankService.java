dc961de2c74e3422ec49e842a89b1fa0c2da84f4
Pavol Loffay <ploffay@redhat.com>
2016-05-24 15:48:11 +0200

    rest logging

--- rest-services/src/main/java/fi/muni/pv207/services/BankService.java
+++ rest-services/src/main/java/fi/muni/pv207/services/BankService.java
@@ -6,16 +6,23 @@ import javax.ws.rs.Path;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * @author Pavol Loffay
  */
 @Path("/")
 public class BankService {
 
+    private static final Logger logger = LoggerFactory.getLogger(BankService.class);
+
     @POST
     @Path("bank")
     @Consumes(MediaType.APPLICATION_JSON)
     public Response sendBack(Double money) {
+        logger.info("Bank request");
+
         return Response.ok().build();
     }
 }
--- rest-services/src/main/java/fi/muni/pv207/services/EmailService.java
+++ rest-services/src/main/java/fi/muni/pv207/services/EmailService.java
@@ -6,6 +6,8 @@ import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Response;
 
 import org.apache.commons.validator.routines.EmailValidator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * @author Pavol Loffay
@@ -14,9 +16,13 @@ import org.apache.commons.validator.routines.EmailValidator;
 @Path("/")
 public class EmailService {
 
+    private static final Logger logger = LoggerFactory.getLogger(EmailService.class);
+
     @GET
     @Path("email")
     public Response checkDiskspace(@QueryParam("email") String email) {
+        logger.info("email=" + email);
+
         EmailValidator emailValidator = EmailValidator.getInstance();
         return emailValidator.isValid(email) ? Response.ok().build() :
                 Response.status(Response.Status.FORBIDDEN).build();
