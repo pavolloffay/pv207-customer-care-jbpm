0056506bbdf48e7bfebcc5d7d74e44b1200e0769
Pavol Loffay <ploffay@redhat.com>
2016-05-24 17:23:57 +0200

    remove user from DB handler

---
+++ customer-care/src/main/java/fi/muni/pv207/registration/handler/RemoveUserFromDBHandler.java
@@ -0,0 +1,32 @@
+package fi.muni.pv207.registration.handler;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.kie.api.runtime.process.WorkItem;
+import org.kie.api.runtime.process.WorkItemHandler;
+import org.kie.api.runtime.process.WorkItemManager;
+
+import fi.muni.pv207.registration.Customer;
+import fi.muni.pv207.registration.db.CustomerDatabase;
+
+/**
+ * @author Pavol Loffay
+ */
+public class RemoveUserFromDBHandler implements WorkItemHandler {
+
+    @Override
+    public void executeWorkItem(WorkItem workItem, WorkItemManager workItemManager) {
+
+        Customer customer = (Customer) workItem.getParameter("Customer");
+
+        CustomerDatabase.customerMap.remove(customer.getEmail());
+
+        Map<String, Object> resultMap = new HashMap<>();
+        workItemManager.completeWorkItem(workItem.getId(), resultMap);
+    }
+
+    @Override
+    public void abortWorkItem(WorkItem workItem, WorkItemManager workItemManager) {
+    }
+}
