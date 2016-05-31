8618527ca6933519e656cb9a241d158c57e0d84c
Jan Faron <Jan Faron>
2016-06-01 00:03:23 +0200

    add new default customer

--- customer-care/src/main/java/fi/muni/pv207/registration/db/CustomerDatabase.java
+++ customer-care/src/main/java/fi/muni/pv207/registration/db/CustomerDatabase.java
@@ -1,10 +1,14 @@
 package fi.muni.pv207.registration.db;
 
 import fi.muni.pv207.registration.Customer;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * @author Pavol Loffay
@@ -14,5 +18,15 @@ public class CustomerDatabase {
     public static Map<String, Customer> customerMap = new HashMap<>();
     static {
         customerMap.put("admin@customercare.com", new Customer("Honza", "admin@customercasre.com", "Novak", new Date(), "PREMIUM"));
+        
+        SimpleDateFormat sdf = new SimpleDateFormat("dd-M-yyyy");
+	String dateInString = "01-01-2017";
+        Date date = new Date();
+        try {
+            date = sdf.parse(dateInString);
+        } catch (ParseException ex) {
+            Logger.getLogger(CustomerDatabase.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        customerMap.put("martin@email.com", new Customer("Martin", "martin@email.com", "Polak", date, "PREMIUM"));
     }
 }
