6298d73b6d50f2b5a25310eec5537d4d187014dd
Pavol Loffay <ploffay@redhat.com>
2016-06-01 15:00:19 +0200

    customer added to BC

--- customer-care/src/main/java/fi/muni/pv207/ticket/handler/SubscriptionHandler.java
+++ customer-care/src/main/java/fi/muni/pv207/ticket/handler/SubscriptionHandler.java
@@ -1,15 +1,16 @@
 package fi.muni.pv207.ticket.handler;
 
-import fi.muni.pv207.registration.Customer;
-import fi.muni.pv207.registration.db.CustomerDatabase;
-import fi.muni.pv207.ticket.BusinessCase;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.drools.core.process.instance.WorkItemHandler;
 import org.kie.api.runtime.process.WorkItem;
 import org.kie.api.runtime.process.WorkItemManager;
 
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
+import fi.muni.pv207.registration.Customer;
+import fi.muni.pv207.registration.db.CustomerDatabase;
+import fi.muni.pv207.ticket.BusinessCase;
 
 public class SubscriptionHandler implements WorkItemHandler {
 
@@ -20,10 +21,13 @@ public class SubscriptionHandler implements WorkItemHandler {
         System.out.println("BUSINESS CASE: " + businessCase);
 
         Customer customer = CustomerDatabase.customerMap.get(businessCase.getEmail());
+        businessCase.customer = customer;
 
         Map<String, Object> resultMap = new HashMap<>();
-        resultMap.put("HasValidSubscription", customer.getSubscriptionTill().after(new Date()));
-        resultMap.put("TypeOfSubscription", customer.getSubscriptionType());
+        resultMap.put("HasValidSubscription", customer != null ?
+                customer.getSubscriptionTill().after(new Date()) : false);
+        resultMap.put("TypeOfSubscription", customer != null ? customer.getSubscriptionType() : null);
+
         resultMap.put("Customer", customer);
         workItemManager.completeWorkItem(workItem.getId(), resultMap);
     }
