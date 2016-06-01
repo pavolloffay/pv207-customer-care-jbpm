82536edd100bdd1b65c58843f18abc18ea9927ab
Pavol Loffay <ploffay@redhat.com>
2016-06-01 13:23:30 +0200

    default user with subscription date

--- customer-care/src/main/java/fi/muni/pv207/registration/db/CustomerDatabase.java
+++ customer-care/src/main/java/fi/muni/pv207/registration/db/CustomerDatabase.java
@@ -1,14 +1,11 @@
 package fi.muni.pv207.registration.db;
 
-import fi.muni.pv207.registration.Customer;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-
+import java.util.Calendar;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+
+import fi.muni.pv207.registration.Customer;
 
 /**
  * @author Pavol Loffay
@@ -17,16 +14,12 @@ public class CustomerDatabase {
 
     public static Map<String, Customer> customerMap = new HashMap<>();
     static {
-        customerMap.put("admin@customercare.com", new Customer("Honza", "admin@customercasre.com", "Novak", new Date(), "PREMIUM"));
-        
-        SimpleDateFormat sdf = new SimpleDateFormat("dd-M-yyyy");
-	String dateInString = "01-01-2017";
-        Date date = new Date();
-        try {
-            date = sdf.parse(dateInString);
-        } catch (ParseException ex) {
-            Logger.getLogger(CustomerDatabase.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        customerMap.put("martin@email.com", new Customer("Martin", "martin@email.com", "Polak", date, "PREMIUM"));
+
+        Calendar c = Calendar.getInstance();
+        c.setTime(new Date()); // Now use today date.
+        c.add(Calendar.DATE, 15); // Adds 15 days
+
+        customerMap.put("admin@customercare.com",
+                new Customer("Honza", "admin@customercare.com", "Novak", c.getTime(), "PREMIUM"));
     }
 }
